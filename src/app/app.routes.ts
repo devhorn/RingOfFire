a404e71129ba0e2a129c5f76b038d615ffb65469
devhorn <mdevhorn@gmail.com>
2025-05-03 20:55:58 +0200

    added start-screen component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { StartScreenComponent } from './start-screen/start-screen.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: StartScreenComponent }];
