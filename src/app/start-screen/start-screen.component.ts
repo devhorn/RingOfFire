909cbaac3c88d1f9ce0c37a6c65f4945e7d5e373
devhorn <mdevhorn@gmail.com>
2025-05-03 21:52:15 +0200

    starting with game screen

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-start-screen',
   standalone: true,
   imports: [],
   templateUrl: './start-screen.component.html',
-  styleUrl: './start-screen.component.scss'
+  styleUrl: './start-screen.component.scss',
 })
 export class StartScreenComponent {
+  constructor(private router: Router) {}
 
+  newGame() {
+    this.router.navigateByUrl('/game');
+  }
 }
