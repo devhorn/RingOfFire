abe78b476a934b797c6758abc77f71b7ea8e7f18
devhorn <mdevhorn@gmail.com>
2025-05-04 20:41:42 +0200

    animate cards

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -8,4 +8,14 @@ import { Component } from '@angular/core';
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss',
 })
-export class GameComponent {}
+export class GameComponent {
+  pickCardAnimation = false;
+
+  trackByIndex(index: number, item: any): number {
+    return index;
+  }
+
+  takeCard() {
+    this.pickCardAnimation = true;
+  }
+}
