5110f76c334bca9b7b34ae042b26a60b848f2e6e
devhorn <mdevhorn@gmail.com>
2025-05-04 21:10:53 +0200

    implement game object

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
@@ -10,12 +11,22 @@ import { Component } from '@angular/core';
 })
 export class GameComponent {
   pickCardAnimation = false;
+  game: Game | undefined;
 
   trackByIndex(index: number, item: any): number {
     return index;
   }
 
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
   takeCard() {
     this.pickCardAnimation = true;
+  }
+
+  ngOnInit(): void {
+    this.newGame();
   }
 }
